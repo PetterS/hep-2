a7915e1102614293bdca06f0f4b53abb6f824e5d
Petter Strandmark <petter.strandmark@gmail.com>
2012-04-28 17:24:50 +0200

    read_image.m

---
+++ read_image.m
@@ -0,0 +1,15 @@
+function [I M] = read_image(image_file,mask_file)
+
+% PCA
+meanI = [ 3.4568 41.4073 0.3621];
+comp  = [0.0592 0.9982 0.0006]; 
+
+I = double(imread(image_file));
+[m n three] = size(I);
+MI = repmat(reshape(meanI,[1 1 3]),[m n 1]);
+I = I - MI;
+I = 255*rgb2gray(reshape(1/255*meanI,[1 1 3])) +  sum(I .* repmat(reshape(comp,[1 1 3]),[m n 1]), 3);
+I = uint8(I);
+
+M = imread(mask_file);
+M=logical(M);

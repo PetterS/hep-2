17ec0c78fd89942390ff59cebad929b7cc195eef
Petter Strandmark <petter.strandmark@gmail.com>
2012-09-26 17:06:12 +0200

    Caching features

--- get_all_features.m
+++ get_all_features.m
@@ -1,22 +1,26 @@
 function [F F_STR] = get_all_features(SET)
-h = waitbar(0,'Computing features... ');
-i=0;
-total_time = 0;
-for id = SET.ID  
-    tic
-		image_file = sprintf('%s%s%03d.png',SET.path,filesep,id);
-		mask_file  = sprintf('%s%s%03d_mask.png',SET.path,filesep,id);
-		[I M] = read_image(image_file, mask_file);
+if exist('features.mat', 'file')
+	load features
+else
+	h = waitbar(0,'Computing features... ');
+	i=0;
+	total_time = 0;
+	for id = SET.ID
+	    tic
+			image_file = sprintf('%s%s%03d.png',SET.path,filesep,id);
+			mask_file  = sprintf('%s%s%03d_mask.png',SET.path,filesep,id);
+			[I M] = read_image(image_file, mask_file);
 
-		[F(i+1,:) F_STR] = get_features(I,M,SET.POS(id)); %#ok<AGROW>
+			[F(i+1,:) F_STR] = get_features(I,M,SET.POS(id)); %#ok<AGROW>
 
-        total_time = total_time + toc;
-	
-	i=i+1;
-	waitbar(i/length(SET.ID), h);
+	        total_time = total_time + toc;
+
+		i=i+1;
+		waitbar(i/length(SET.ID), h);
+	end
+	close(h);
+
+	fprintf('Avg. time for loading image and computing features: %.2fs\n',total_time/length(SET.ID));
+
+	save features F F_STR
 end
-close(h);
-
-fprintf('Avg. time for loading image and computing features: %.2fs\n',total_time/length(SET.ID));
-
-save features F F_STR
